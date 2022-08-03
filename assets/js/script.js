1ea9bef6849a6d582719c2bf7148422e0f282182
y0kem <62028312+y0kem@users.noreply.github.com>
2022-08-03 22:17:59 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var count = 1;
+  $('.mobile div').on('click', function () {
+      $('.mobile div').toggleClass('active');
+  
+      if (count % 2 == 0) {
+          $('.mobile div span').removeClass("backgr-black").addClass("backgr-white");
+          count++;
+      } else {
+          $('.mobile div span').removeClass("backgr-white").addClass("backgr-black");
+          count++;
+      } // $('.mobile div span').addClass('backgr-white');
+  
+  
+      $('.mobile nav').toggleClass('open');
+      $('.mobile nav ul').toggleClass('show');
+  });
+  
+  $('.mobile nav ul').on('click', function () {
+      $('.mobile div').toggleClass('active');
+      $('.mobile nav').removeClass('open');
+  });
+  
+  $('a.open-modal').click(function(event) {
+      $(this).modal({
+        fadeDuration: 250
+      });
+      return false;
+    });
